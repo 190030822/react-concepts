d00ad3e19ff51662e69875e254a09e557f33cf06
190030822 <190030822@kluniversity.in>
2023-12-25 17:01:19 +0530

    props example -1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+let moviesData = [
+  {
+    title: "Avatar: The Last Airbender",
+    movie_image: "https://sm.ign.com/ign_nordic/cover/a/avatar-gen/avatar-generations_prsz.jpg",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, nulla?"
+  },
+  {
+    title: "Toy Story",
+    movie_image: "https://www.yardbarker.com/media/d/0/d087c897fc875471933ced712bf4677d8b551a3a/thumb_16x9/28-best-animated-movie-franchises-time.jpg",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, nulla?"
+  },
+  {
+    title: "The Light House",
+    movie_image: "https://www.keyframeschool.mx/wp-content/uploads/2019/02/up.jpg",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, nulla?"
+  },
+  {
+    title: "The Ghost School",
+    movie_image: "https://wallpapercave.com/dwp1x/wp2383818.jpg",
+    desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Incidunt, nulla?"
+  },
+]
+
+
+const Movie = (props) => {
+  const {title, image, desc} = props;
+  console.log(image);
+  return (
+    <article className = "movie-article">
+      <figure className="img-container">
+        <img className = "main-image" src = {image} alt = "Avatar pic" />
+        <figcaption className = "sub-heading">{title}</figcaption>
+    </figure>
+    <p>{desc}</p>
+  </article>
+  );
+}
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className = "flex-container"> {
+      moviesData.map((movie, index) => {
+        const {title, movie_image: image, desc} = movie;
+        return (<Movie key = {index} title = {title} image = {image} desc = {desc}/>);
+      })
+    }
+      
+    </section>
+   
   );
 }
 
