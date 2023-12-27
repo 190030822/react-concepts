ae96c9867ff99054d15c675d34050a9396f69421
190030822 <190030822@kluniversity.in>
2023-12-27 12:28:47 +0530

    use effect implemented

--- src/Increment.js
+++ src/Increment.js
@@ -1,58 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Comment from "./Comment";
 import CreatePost from "./CreatePost";
 
 export const Increment = () => {
     
-    const posts = [
-        {
-            "userId": 1,
-            "id": 1,
-            "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-        },
-        {
-        "userId": 1,
-        "id": 2,
-        "title": "qui est esse",
-        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-        },
-        {
-        "userId": 1,
-        "id": 3,
-        "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-        },
-        {
-        "userId": 1,
-        "id": 4,
-        "title": "eum et est occaecati",
-        "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
-        },
-        {
-        "userId": 1,
-        "id": 5,
-        "title": "nesciunt quas odio",
-        "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-        },
-        {
-        "userId": 1,
-        "id": 6,
-        "title": "dolorem eum magni eos aperiam quia",
-        "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
-        }
-    ];
-
-    const [data, setData] = useState(posts);
-
     const defaultPostData = {
-        "userId" : 1,
-        "id" : 0,
-        "title" : "",
-        "body" : ""
+        "idDrink" : 0,
+        "strDrink" : "",
+        "strGlass" : "",
+        "strVideo" : ""
     };
 
     const [newPost, setNewPost] = useState(defaultPostData);
+    const [data, setData] = useState([]);
+    const [isLoading, setLoadingState] = useState(false);
+    const [searchingDrinkName, setSearchingDrinkName ] = useState("");
+
+    const postsUrl = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+    const fetchPosts = async (postApi)  => {
+        try {
+            setLoadingState(true);
+            const response = await fetch(postApi+searchingDrinkName);
+            const data = await response.json();
+            setData(data.drinks);
+            setLoadingState(false);
+        } catch (e) {
+
+        }
+   }
+
+   useEffect(() => {
+    fetchPosts(postsUrl);
+   }, [searchingDrinkName]);
 
 
 
@@ -82,15 +62,30 @@ export const Increment = () => {
 
     return (
         <section>
+            {
+                        console.log(data)
+            }
             <section>
-                <CreatePost postData = {newPost} postCreation = {addPost} />
+                <div>
+                    <input type = "text" name = "drink_name" value = {searchingDrinkName} onChange = {(e) => setSearchingDrinkName(e.target.value)} />
+                </div>
             </section>
-           {
-            data.map((post) => {
-                const {id, title, body} = post;
-                return <Comment key = {id} id = {id} title = {title} body = {body} deletePost = {deletePost}  updatePost = {updatePost} />
-            })
-           }
+            <section >
+                {
+                    isLoading ? <center>Is Loading ----</center> : ( data === null ? <center>Search for Drinks</center> : 
+                    <main className = "drinks-container">
+                    {
+                        data.map((post) => {
+                            const {idDrink,  strDrinkThumb } = post;
+                            return <img className = "drink" key = {idDrink} src = {strDrinkThumb} alt = {idDrink}/>
+                        }) 
+                    }
+                    </main>
+                    )
+                        
+                }
+            </section>
         </section>
     );
 }
+
